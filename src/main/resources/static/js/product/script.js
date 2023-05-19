eb8ebc3384746fdf99820dafc5f19d436aa4318c
Putilov Dmitrii <putilovdmitrii@Putilovs-MacBook-Air.local>
2023-05-19 20:35:04 +0300

    init commit

---
+++ src/main/resources/static/js/product/script.js
@@ -0,0 +1,11 @@
+function searchShow(){
+  let searchField = document.getElementById("search");
+  if(searchField.style.display == "block"){   
+    searchField.style.display = "none"
+  }
+  else{
+    searchField.style.display = "block"
+
+  }
+  
+}
